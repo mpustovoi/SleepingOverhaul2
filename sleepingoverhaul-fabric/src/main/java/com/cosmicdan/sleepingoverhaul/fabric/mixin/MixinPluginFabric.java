23d08544368fdbfaf32671b2c77f38431b70e9c4
CosmicDan <dconnolly.au@gmail.com>
2023-09-21 20:05:53 +1000

    Finish Fabric mixins

---
+++ sleepingoverhaul-fabric/src/main/java/com/cosmicdan/sleepingoverhaul/fabric/mixin/MixinPluginFabric.java
@@ -0,0 +1,68 @@
+package com.cosmicdan.sleepingoverhaul.fabric.mixin;
+
+import dev.architectury.platform.Platform;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author Daniel 'CosmicDan' Connolly
+ */
+public class MixinPluginFabric implements IMixinConfigPlugin {
+    private static final String[] MIXINS_COMMON = {
+            "PlayerDaycheckHook",
+            "ServerPlayerDaycheckHook"
+    };
+    private static final String[] MIXINS_CLIENT_ONLY = {
+
+    };
+    private static final String[] MIXINS_SERVER_ONLY = {
+
+    };
+
+    @Override
+    public void onLoad(String mixinPackage) {
+        //InjectionInfo.register(ModifyConstantCheckedInjectionInfo.class);  // @ModifyConstantChecked
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        return true;
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+
+    }
+
+    @Override
+    public List<String> getMixins() {
+        final List<String> mixinsToLoad = new ArrayList<>(8);
+        mixinsToLoad.addAll(Arrays.asList(MIXINS_COMMON));
+        switch (Platform.getEnvironment()) {
+            case CLIENT -> mixinsToLoad.addAll(Arrays.asList(MIXINS_CLIENT_ONLY));
+            case SERVER -> mixinsToLoad.addAll(Arrays.asList(MIXINS_SERVER_ONLY));
+        }
+        return mixinsToLoad;
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+    }
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+    }
+}
