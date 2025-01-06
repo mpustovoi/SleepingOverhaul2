1b0052ae0491cc316af6284035267d8bf423eb23
CosmicDan <dconnolly.au@gmail.com>
2025-01-06 17:13:10 +1100

    Only apply Timelapse cinematic camera to reallySleeping players

--- sleepingoverhaul-common/src/main/java/com/cosmicdan/sleepingoverhaul/mixin/injection/TimelapseMixinsCommonClient.java
+++ sleepingoverhaul-common/src/main/java/com/cosmicdan/sleepingoverhaul/mixin/injection/TimelapseMixinsCommonClient.java
@@ -2,6 +2,7 @@ package com.cosmicdan.sleepingoverhaul.mixin.injection;
 
 import com.cosmicdan.sleepingoverhaul.SleepingOverhaul;
 import com.cosmicdan.sleepingoverhaul.client.ClientConfig;
+import com.cosmicdan.sleepingoverhaul.mixin.proxy.PlayerMixinProxy;
 import com.cosmicdan.sleepingoverhaul.server.ServerConfig;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
@@ -13,6 +14,7 @@ import net.minecraft.client.player.Input;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraft.world.phys.Vec3;
@@ -50,38 +52,40 @@ abstract class TimelapseMixinsCommonClientCamera {
             at = @At("TAIL"),
             require = 1, allow = 1
     )
-    private void afterCameraSetup(final BlockGetter levelIn, final Entity cameraEntity, final boolean isThirdPerson, final boolean isMirrored, final float partialTicks, final CallbackInfo ci) {
-        final int cineStage = SleepingOverhaul.clientState.getTimelapseCinematicStage();
-        if (cineStage == 1) {
-            // just skip to next cine stage for now (only did this for a transition animation originally but meh)
-            Minecraft.getInstance().gameRenderer.setPanoramicMode(false);
-            SleepingOverhaul.clientState.advanceTimelapseCinematicStage();
-            previousMaxHeight = -1.0;
-        } else if (cineStage == 2) {
-            final ClientConfig.TimelapseCameraType timelapseCameraType = SleepingOverhaul.clientConfig.timelapseCameraType.get();
-            if ((timelapseCameraType != ClientConfig.TimelapseCameraType.None) && (levelIn instanceof ClientLevel level)) {
-                // level.getSunAngle is weird, so we calculate it ourselves
-                final float timeOfDayAsFraction = (level.getDayTime() % 24000L) / 24000.0f;
+    private void afterCameraSetup(final BlockGetter levelIn, final Entity entity, final boolean isThirdPerson, final boolean isMirrored, final float partialTicks, final CallbackInfo ci) {
+        if (entity instanceof Player player) {
+            final int cineStage = SleepingOverhaul.clientState.getTimelapseCinematicStage();
+            if (cineStage == 1) {
+                // just skip to next cine stage for now (only did this for a transition animation originally but meh)
+                Minecraft.getInstance().gameRenderer.setPanoramicMode(false);
+                SleepingOverhaul.clientState.advanceTimelapseCinematicStage();
+                previousMaxHeight = -1.0;
+            } else if (cineStage == 2 && ((PlayerMixinProxy) player).isReallySleeping()) {
+                final ClientConfig.TimelapseCameraType timelapseCameraType = SleepingOverhaul.clientConfig.timelapseCameraType.get();
+                if ((timelapseCameraType != ClientConfig.TimelapseCameraType.None) && (levelIn instanceof ClientLevel level)) {
+                    // level.getSunAngle is weird, so we calculate it ourselves
+                    final float timeOfDayAsFraction = (level.getDayTime() % 24000L) / 24000.0f;
 
-                // rotate camera first
-                setRotation((timeOfDayAsFraction * 360.0f * 2.0f) - 90, 0.0f);
+                    // rotate camera first
+                    setRotation((timeOfDayAsFraction * 360.0f * 2.0f) - 90, 0.0f);
 
-                if (timelapseCameraType == ClientConfig.TimelapseCameraType.SurfaceOrbit) {
-                    // SurfaceOrbit: Move camera back a bit
-                    move(-6.0, 1.0, 0.0);
+                    if (timelapseCameraType == ClientConfig.TimelapseCameraType.SurfaceOrbit) {
+                        // SurfaceOrbit: Move camera back a bit
+                        move(-6.0, 1.0, 0.0);
+                    }
+                    if ((timelapseCameraType == ClientConfig.TimelapseCameraType.SurfaceOrbit) || (timelapseCameraType == ClientConfig.TimelapseCameraType.SurfaceRotation)) {
+                        // SurfaceOrbit *or* SurfaceRotation: move camera to surface block + 3 (unless lower than previous surface height in the current cine)
+                        final BlockPos topmostPosition = level.getHeightmapPos(Heightmap.Types.WORLD_SURFACE, getBlockPosition()).above(3);
+                        if (topmostPosition.getY() > previousMaxHeight)
+                            previousMaxHeight = topmostPosition.getY();
+                        setPosition(new Vec3(position.x(), previousMaxHeight, position.z()));
+                    }
                 }
-                if ((timelapseCameraType == ClientConfig.TimelapseCameraType.SurfaceOrbit) || (timelapseCameraType == ClientConfig.TimelapseCameraType.SurfaceRotation)) {
-                    // SurfaceOrbit *or* SurfaceRotation: move camera to surface block + 3 (unless lower than previous surface height in the current cine)
-                    final BlockPos topmostPosition = level.getHeightmapPos(Heightmap.Types.WORLD_SURFACE, getBlockPosition()).above(3);
-                    if (topmostPosition.getY() > previousMaxHeight)
-                        previousMaxHeight = topmostPosition.getY();
-                    setPosition(new Vec3(position.x(), previousMaxHeight, position.z()));
-                }
+            } else if (cineStage == 3) {
+                previousMaxHeight = -1.0;
+                Minecraft.getInstance().gameRenderer.setPanoramicMode(false);
+                SleepingOverhaul.clientState.advanceTimelapseCinematicStage();
             }
-        } else if (cineStage == 3) {
-            previousMaxHeight = -1.0;
-            Minecraft.getInstance().gameRenderer.setPanoramicMode(false);
-            SleepingOverhaul.clientState.advanceTimelapseCinematicStage();
         }
     }
 }
